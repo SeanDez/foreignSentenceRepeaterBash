87579e68cfae88ef5b5fad8816eaeeb221eb4dff
Sean Dez <sd@seandezoysa.com>
2020-05-19 06:56:22 +0700

    fix setup comments

--- runScript.sh
+++ runScript.sh
@@ -1,13 +1,13 @@
 # ---------------- Initial setup on Google Cloud
 
 # 1) follow the instructions on this page to create a credentials.json file and save it to the project root
-# https://console.cloud.google.com/apis/credentials?project=buyusedshopify
+# https://cloud.google.com/docs/authentication/getting-started
 
-# 2) Enable the Text-to-Speech API: https://console.developers.google.com/apis/api/texttospeech.googleapis.com/overview?project=119445587202
+# 2) Enable the Text-to-Speech API: https://console.developers.google.com/apis/api/texttospeech.googleapis.com/overview
 
-# 3) Enable the Translations API: https://console.developers.google.com/apis/api/translate.googleapis.com/overview?project=119445587202
+# 3) Enable the Translations API: https://console.developers.google.com/apis/api/translate.googleapis.com/overview
 
-# Language code: https://developers.google.com/admin-sdk/directory/v1/languages
+# Language codes: https://developers.google.com/admin-sdk/directory/v1/languages
 
 # ------------------ Script Execution
 
