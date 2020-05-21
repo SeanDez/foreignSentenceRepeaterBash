4dddb4b3fefa6bb672c4f4040b434d54dc427995
Sean Dez <sd@seandezoysa.com>
2020-05-21 15:41:53 +0700

    add bats and 4 support libraries

--- runScript.sh
+++ runScript.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # ---------------- Initial setup on Google Cloud
 
 # 1) follow the instructions on this page to create a credentials.json file and save it to the project root
@@ -9,6 +11,54 @@
 
 # Language codes: https://developers.google.com/admin-sdk/directory/v1/languages
 
+
+# --------------- Config Variable Declaration
+declare -A configData
+
+# --------------- Function Definitions
+
+function setConfigurations {
+   read -p "Enter an absolute or relative path for the parent folder where all audios will be saved: " configData[outputParentFolder]
+
+   # If there is no config file, create it.
+   if [[ ! -f "${configData[outputParentFolder]}/courseConfigurations.txt" ]]
+   then
+      touch ./courseConfigurations.txt
+      chmod 644 ./courseConfigurations.txt # read write
+   fi
+
+
+   read -p "Please go to the following url (in your web browser) to find your desired language's code. Then enter the code here. ( https://developers.google.com/admin-sdk/directory/v1/languages ): " configData[languageCode]
+
+
+   read -p "How many times should the sentence, and all words in the sentence, repeat? (2 or 3 times is recommended): " configData[numberOfRepeats]
+
+   return 0 # success
+}
+
+
+function buildSentenceFolder {
+   local $sentenceString = $1
+
+   ### build the specific sentence folder name
+   # split string into array on \s
+   for word in $sentenceString
+   do
+      # capitalize each word
+      echo
+      # remove spaces
+      # create titlecaps string (folder name)
+   done 
+
+   # create a folder in the specified config directory
+}
+
+function buildSentenceAudios {
+   echo
+}
+
+
+
 # ------------------ Script Execution
 
 # setup the required Google Env variable for their SDK to load credentials
@@ -17,10 +67,37 @@ export GOOGLE_APPLICATION_CREDENTIALS="./credentials.json"
 echo "Using credentials file at ${GOOGLE_APPLICATION_CREDENTIALS}"
 
 
+# case statement to control all options
+while [[ $# -gt 0 ]];
+do 
+   case $1 in 
+      -c|--config)
+         # setConfigurations $1
+         shift
+         ;;
+      *)
+         # default case. Expects a sentence string
+         buildAudios $1 
+         shift
+         ;;
+   esac
+done
+
+echo ${configData[languageCode]}
+echo ${configData[outputParentFolder]}
+echo ${configData[numberOfRepeats]}
+
+
+
+
+
+
+
+
 
 # run the main script
 
-echo "Running the main script file using node..."
-node ./audioDl.js
+# echo "Running the main script file using node..."
+# node ./audioDl.js
 
 
